d4ca2036e24a6f34dfe272659b11062416188709
Jonas Trogen <jonas.trogen@husfoto.se>
2022-11-12 20:35:01 +0100

    DynamoDB service

---
+++ backend/services/blogpostdynamodbservice.go
@@ -0,0 +1,101 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"blog.jonastrogen.se/models"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+type BlogpostDynamoDBService struct {
+	db    *dynamodb.Client
+	table string
+}
+
+func NewBlogpostDynamoDBService(table string, cfg aws.Config) *BlogpostDynamoDBService {
+	return &BlogpostDynamoDBService{
+		db:    dynamodb.NewFromConfig(cfg),
+		table: table,
+	}
+}
+
+func (s *BlogpostDynamoDBService) GetBlogpost(ctx context.Context, name string) ([]byte, error) {
+	titleAttr, err := attributevalue.Marshal("test")
+	if err != nil {
+		return nil, err
+	}
+	result, err := s.db.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(s.table),
+		Key: map[string]types.AttributeValue{
+			"title": titleAttr,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if result.Item == nil {
+		msg := "Could not find '" + name + "'"
+		return nil, errors.New(msg)
+	}
+
+	// TODO
+	return []byte{}, nil
+}
+
+func (s *BlogpostDynamoDBService) GetMetadata(ctx context.Context, key string) (*models.Metadata, error) {
+	titleAttr, err := attributevalue.Marshal("test")
+	if err != nil {
+		return nil, err
+	}
+	result, err := s.db.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(s.table),
+		Key: map[string]types.AttributeValue{
+			"title": titleAttr,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if result.Item == nil {
+		msg := "Could not find '" + key + "'"
+		return nil, errors.New(msg)
+	}
+
+	metadata := models.Metadata{}
+
+	err = attributevalue.UnmarshalMap(result.Item, &metadata)
+	if err != nil {
+		return nil, err
+	}
+
+	return &metadata, nil
+}
+
+func (s *BlogpostDynamoDBService) ListMetadata(ctx context.Context) (*[]models.Metadata, error) {
+	files, err := os.ReadDir("blogposts/")
+	if err != nil {
+		return nil, err
+	}
+
+	// WIP fix this
+	metadataList := []models.Metadata{}
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".json" {
+			metadata, err := s.GetMetadata(ctx, fmt.Sprintf("blogposts/%s", file.Name()))
+			if err != nil {
+				return nil, err
+			}
+			metadataList = append(metadataList, *metadata)
+		}
+		fmt.Println(file.Name(), file.IsDir())
+	}
+
+	return &metadataList, err
+}

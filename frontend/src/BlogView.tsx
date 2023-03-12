6738362bf01804c5393f80b0530f9f319044dcec
Jonas Trogen <jonas.trogen@husfoto.se>
2023-03-12 11:25:32 +0100

    Fetch metadata, layout

--- frontend/src/BlogView.tsx
+++ frontend/src/BlogView.tsx
@@ -6,14 +6,17 @@ import { useParams } from 'solid-app-router';
 const BlogListView: Component = (props) => {
   const params = useParams();
   const fetchBlogPost = async (id: string) => (await fetch(`http://localhost:3001/api/blogposts/${id}`)).text()
+  const fetchMetadata = async (id: string) => (await fetch(`http://localhost:3001/api/metadata/${id}`)).json() as unknown as IMetadata
   const [blogPost] = createResource(params.id, fetchBlogPost);
-  // onMount(async () => {
-  //     const res = await fetch(`http://localhost:3001/api/blogposts/${params.id}`);
-  //     setBlogPost(await res.text());
-  //   });
-      
+  const [metadata] = createResource(params.id, fetchMetadata);
+  
   return (
     <div id="blog-post">
+      <a class="back" href="/">Back to blog posts</a>
+      <h1>{metadata()?.title}</h1>
+      <div class="preamble">{metadata()?.preamble}</div>
+      <div class="date-category">Posted at {metadata()?.publishDate} in {metadata()?.category}</div>
+      <div class="tags">{metadata()?.tags.map(t => `#${t} `)}</div>
       <div innerHTML={blogPost()}></div>
     </div>
   );
